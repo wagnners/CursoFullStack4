9ef3a61ca3f7c8316eb59b349d55b9b23bfc3233
Wagner Luz <wagnnersousa@hotmail.com>
2020-10-26 22:14:19 -0300

    Personalizado Layout 2

--- PrimeroProjetoMVC/config/helpers.php
+++ PrimeroProjetoMVC/config/helpers.php
@@ -11,7 +11,7 @@
 // define('LOCAL_URL', '/PrimeiroProjetoFullStack');
 
 if($env == "dev"){
-	define('LOCAL_URL', '/PrimeroProjetoMVC');
+	define('LOCAL_URL', '/FullStack4/PrimeroProjetoMVC'); // Aqui você arruma a pasta do seu projeto que está dentro do HTDOCS
 }
 elseif($env == "prod"){
 	define('LOCAL_URL', 'https://www.google.com.br/');
