aa3269487115440b23fd7d9f954ab767d6560679
Wagner Luz <wagnnersousa@hotmail.com>
2020-11-26 22:03:45 -0300

    Continuação Aula 28

--- aula28/src/index.js
+++ aula28/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
+import './index.css';
+import meuStilo from './my_style.module.css';
+import './style_sass.scss';
 
 // class Car extends React.Component {
   
@@ -62,37 +64,101 @@ import ReactDOM from 'react-dom';
 //   }
 // }
 
-class Togle extends React.Component {
+// class Togle extends React.Component {
+  
+//   constructor(props){
+
+//     super(props);
+
+//     this.state = {
+//       isOn: false
+//     };
+  
+//   }
+
+//   handleClick = () => {
+//     this.setState(
+//       {
+//         isOn : !this.state.isOn
+//       }
+//     )
+//   }
+
+//   render(){
+//     return (
+//       <button onClick={this.handleClick}>
+//         {this.state.isOn ? "ON" : "OFF"}
+//       </button>
+//     )
+//   }
+// }
+
+class Formulario extends React.Component {
   
   constructor(props){
 
     super(props);
 
     this.state = {
-      isOn: false
+      nome: "",
+      sobrenome: "",
+      fruta: "laranja",
+      status: true
     };
   
   }
 
-  handleClick = () => {
-    this.setState(
-      {
-        isOn : !this.state.isOn
-      }
-    )
+  handleSubmit = (event) => {
+    console.log(this.state);
+    event.preventDefault();
+  }
+
+  
+  handleInputChange = (event) => {
+    
+    const value = event.target.name === "status" ? event.target.checked : event.target.value;
+
+    this.setState({
+      [event.target.name] : value
+    })
+   
   }
 
   render(){
+
+    const labelCSS = {
+      display: "inherit",
+      padding: "5px 0px"
+    };
+
     return (
-      <button onClick={this.handleClick}>
-        {this.state.isOn ? "ON" : "OFF"}
-      </button>
+      <> 
+        <h2 style={{backgroundColor: "lightblue", fontSize: "30px"}} >Cadastro de Pessoa</h2>
+        <form onSubmit={this.handleSubmit} className={meuStilo.fomulario}>
+          <label style={labelCSS}>Nome: </label>
+          <input type="text" name="nome" className="input" value={this.state.nome} onChange={this.handleInputChange}></input>
+          <label style={labelCSS}>Sobrenome: </label>
+          <input type="text" name="sobrenome" className="input" value={this.state.sobrenome} onChange={this.handleInputChange}></input>
+          <label style={labelCSS}>Fruta Favorita: </label>
+          <select name="fruta" value={this.state.fruta} className="input" onChange={this.handleInputChange}>
+            <option value="laranja">Laranja</option>
+            <option value="limao">Limão</option>
+            <option value="coco">Coco</option>
+            <option value="manga">Manga</option>
+          </select>
+          <label>Status: </label>
+          <input name="status" type="checkbox" checked={this.state.status} onChange={this.handleInputChange}></input>
+          <br/>
+          <br/>
+          <input type="submit" value="Salvar"></input>
+        </form>
+      </>
     )
   }
 }
 
 ReactDOM.render(
-  <Togle />,
+  <Formulario />,
   document.getElementById('root')
 );
 
