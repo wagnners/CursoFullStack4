26a199dcc51895987a8edecc87b935cd0dbce382
Wagner Luz <wagnnersousa@hotmail.com>
2020-12-09 21:18:00 -0300

    Atualizado Páginas

---
+++ ProjetoMVC_ASP/ProjetoMVC_ASP/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoMVC_ASP.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About(int id, string nome)
+        {
+            ViewBag.Message = id;
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+
+            return View();
+        }
+    }
+}
